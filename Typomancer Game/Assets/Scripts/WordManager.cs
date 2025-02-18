e0cfcffe83d51bfa358fda5444b2f1ee0e21187f
VervaekeMilan <milan.vervaeke@student.howest.be>
2025-02-18 13:14:59 +0100

    fixing github

--- Typomancer Game/Assets/Scripts/WordManager.cs
+++ Typomancer Game/Assets/Scripts/WordManager.cs
@@ -6,10 +6,56 @@ public class WordManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<SO_Word> _wordList = new List<SO_Word>();
+    UIWordSelector UI;
     void Start()
     {
         LoadDictionary();
         Debug.Log(_wordList.Count);
+
+        UI = FindObjectOfType<UIWordSelector>();
+        UI.OnValidateWord += ValidateWord;
+    }
+
+    private void ValidateWord(object sender, WordEventArgs e)
+    {
+        if (IsWordInDictionary(GetWordFromEvent(e)))
+        {
+            foreach (var letter in e.Word)
+            {
+                letter.color = Color.green;
+            }
+
+        }
+        else
+        {
+            foreach (var letter in e.Word)
+            {
+                letter.color = Color.black;
+            }
+        }
+    }
+
+    private bool IsWordInDictionary(string spelledWord)
+    {
+        foreach (var word in _wordList)
+        {
+            if (word.Word.ToLower() == spelledWord.ToLower())
+            {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private string GetWordFromEvent(WordEventArgs e)
+    {
+        List<char> Letters = new();
+        foreach (var letter in e.Word)
+        {
+            Letters.Add(letter.text.ToCharArray()[0]);
+        }
+        return new string(Letters.ToArray());
     }
 
     private void LoadDictionary()
